82cb1d4e27054dd5f4befdc8bc5ae2b553ce8baf
Vanshul Agarwal <vanshulagarwal2004@gmail.com>
2024-04-12 12:45:13 +0530

    packages installed

--- server/app.js
+++ server/app.js
@@ -1,5 +1,30 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const dbUrl = process.env.ATLAS_URL || 'mongodb://127.0.0.1:27017/SanjeevYatra';
+mongoose.connect(dbUrl)
+    .then(() => {
+        console.log('mongo database connected');
+    })
+    .catch((err) => {
+        console.log('mongo connection error!!');
+        console.log(err);
+    })
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 
 app.get('/', (reeq, res, next) => {
     res.send("hello");
